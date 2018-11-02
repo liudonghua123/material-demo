361478b2acaa470e20d7ffa56e4a087d35933fff
liudonghua123 <liudonghua123@gmail.com>
2018-11-02 15:26:56 +0800

    initial electron files

---
+++ preact.config.js
@@ -0,0 +1,9 @@
+
+export default (config, env, helpers) => {
+	config.target = 'electron-renderer';
+	if (env.production) {
+		config.output.publicPath = env.pkg.homepage;
+		let { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
+		plugin.definitions.PUBLIC_PATH = env.pkg.homepage;
+	}
+};
