08ce395ba13b9e74d00f0b1cf2fcfcbf1b4145e8
liudonghua123 <liudonghua123@gmail.com>
2018-11-02 16:06:22 +0800

    Fix: ERROR polyfills.js from UglifyJs Unexpected token: name (Blob)

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,4 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 export default (config, env, helpers) => {
 	config.target = 'electron-renderer';
@@ -5,5 +6,11 @@ export default (config, env, helpers) => {
 		config.output.publicPath = env.pkg.homepage;
 		let { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
 		plugin.definitions.PUBLIC_PATH = env.pkg.homepage;
+	}
+	
+	const uglifyJsPlugin = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+	if (uglifyJsPlugin) {
+		const { index } = uglifyJsPlugin;
+		config.plugins.splice(index, 1, new UglifyJsPlugin());
 	}
 };
